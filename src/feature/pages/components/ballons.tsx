51c178e4a7831c0fea77ad2588b84c24252e4591
CristianDev <camilovargas_12@misena.edu.co>
2025-10-11 17:27:57 -0500

    Fix:setting several

--- src/feature/pages/components/ballons.tsx
+++ src/feature/pages/components/ballons.tsx
@@ -10,8 +10,8 @@ export const Ballons = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   return (
-    <section className="py-5  text-center px-4 md:px-16 overflow-hidden relative">
-      <h2 className="text-3xl font-bold text-purple-700 mb-14 font-['Cinzel'] tracking-wide">
+    <section className="py-8  text-center px-4 md:px-16 overflow-hidden relative">
+      <h2 className="text-3xl font-bold text-purple-700 mb-5 font-['Cinzel'] tracking-wide">
         Globos con Helio - Domicilio
       </h2>
 
